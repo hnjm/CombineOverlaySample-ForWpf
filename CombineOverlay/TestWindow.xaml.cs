4bbb26fc6c706f098cb4477a6d2affc507c1f63d
ScottZhou <ScottZhou@thinkgeo.com>
2018-12-27 13:14:17 +0800

    The background map use ThinkGeoCloudMapsOverlay

--- CombineOverlay/TestWindow.xaml.cs
+++ CombineOverlay/TestWindow.xaml.cs
@@ -24,12 +24,13 @@ namespace CombiningOverlays
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             //Sets the correct map unit and the extent of the map.
-            wpfMap1.MapUnit = GeographyUnit.DecimalDegree;
-            wpfMap1.CurrentExtent = new RectangleShape(-131.22, 55.05, -54.03, 16.91);
+            wpfMap1.MapUnit = GeographyUnit.Meter;
+            wpfMap1.ZoomLevelSet = ThinkGeoCloudMapsOverlay.GetZoomLevelSet();
+            wpfMap1.CurrentExtent = new RectangleShape(-14607343.5818934, 7371576.14679691, -6014592.08756057, 1910351.0222467);
 
-            //Adds the WorldMapKit as a background.
-            WorldMapKitWmsWpfOverlay worldMapKitOverlay = new WorldMapKitWmsWpfOverlay();
-            wpfMap1.Overlays.Add(worldMapKitOverlay);
+            // Add ThinkGeoCloudMapsOverlay as basemap
+            ThinkGeoCloudMapsOverlay baseOverlay = new ThinkGeoCloudMapsOverlay();
+            wpfMap1.Overlays.Add(baseOverlay);
 
             //Adds the Shapefile MajorCities as a ShapeFileFeatureLayer between zoom levels 01 and 04.
             ShapeFileFeatureLayer shapeFileFeatureLayer = new ShapeFileFeatureLayer(@"..\..\Data\MajorCities.shp");
@@ -47,12 +48,12 @@ namespace CombiningOverlays
             markerOverlay.FeatureSource = shapeFileFeatureSource;
 
             //Here we take advantage of the many properties of MarkerOverlay such as ImageSource and Tooltip.
-            markerOverlay.ZoomLevelSet.ZoomLevel04.DefaultPointMarkerStyle.ImageSource = new BitmapImage(new Uri("/Resources/AQUA.png", UriKind.RelativeOrAbsolute));
-            markerOverlay.ZoomLevelSet.ZoomLevel04.DefaultPointMarkerStyle.Width = 20;
-            markerOverlay.ZoomLevelSet.ZoomLevel04.DefaultPointMarkerStyle.Height = 34;
-            markerOverlay.ZoomLevelSet.ZoomLevel04.DefaultPointMarkerStyle.YOffset = -17;
-            markerOverlay.ZoomLevelSet.ZoomLevel04.DefaultPointMarkerStyle.ToolTip = "This is [#AREANAME#].";
-            markerOverlay.ZoomLevelSet.ZoomLevel04.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+            markerOverlay.ZoomLevelSet.ZoomLevel03.DefaultPointMarkerStyle.ImageSource = new BitmapImage(new Uri("/Resources/AQUA.png", UriKind.RelativeOrAbsolute));
+            markerOverlay.ZoomLevelSet.ZoomLevel03.DefaultPointMarkerStyle.Width = 20;
+            markerOverlay.ZoomLevelSet.ZoomLevel03.DefaultPointMarkerStyle.Height = 34;
+            markerOverlay.ZoomLevelSet.ZoomLevel03.DefaultPointMarkerStyle.YOffset = -17;
+            markerOverlay.ZoomLevelSet.ZoomLevel03.DefaultPointMarkerStyle.ToolTip = "This is [#AREANAME#].";
+            markerOverlay.ZoomLevelSet.ZoomLevel03.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
 
             wpfMap1.Overlays.Add("MarkerOverlay", markerOverlay);   
 
